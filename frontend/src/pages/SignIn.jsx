38583024a530aee61cdd1c4753a0ec9c97f33ae5
Pranava <pranavakp2004@gmail.com>
2025-09-21 19:13:44 +0530

    temp

---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SignIn() {
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      {/* You can add your sign-in form here */}
+    </div>
+  );
+}
+
+export default SignIn;
